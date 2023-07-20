db02d3dc580e0e93ae10d4dde68d33fa03526ebc
Jonathan Bijl <sannoayo@gmail.com>
2023-07-20 13:47:09 -0400

    Test the route with Postman for the projects

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ const app = express();
 require("./config")(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+const allRoutes = require("./routes/index.routes");
+app.use("/api", allRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+const projectRouter = require("./routes/project.routes");     // <== IMPORT
+app.use("/api", projectRouter);                           // <== ADD
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
